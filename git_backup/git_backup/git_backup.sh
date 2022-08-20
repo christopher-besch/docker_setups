31155cd7388e2d1d1ecc546731d30f4e51f74ef4
christopher-besch <mail@chris-besch.com>
2022-08-20 21:24:14 +0200

    more work on git_backup

--- git_backup/git_backup/git_backup.sh
+++ git_backup/git_backup/git_backup.sh
@@ -6,18 +6,23 @@
 set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-LOG="/var/lib/git_backup/temp/git_backup.log"
+
+GIT_BACKUP_DIR="/var/lib/git_backup"
+TEMP_DIR="$GIT_BACKUP_DIR/temp"
+BORG_REPO="$GIT_BACKUP_DIR/repo"
+CONFIGS_DIR="$GIT_BACKUP_DIR/configs"
 # used to clone repos into ->
 # makes time where the actual repo target dir is present but not properly cloned yet as short as possible ->
 # crashes don't create zombie dirs that can't be git pulled or cloned
-CLONE_TARGET="/var/lib/git_backup/temp/clone_target"
+CLONE_TARGET="$TEMP_DIR/clone_target"
+LOG="$TEMP_DIR/git_backup.log"
 
 clone_repo() {
     echo "cloning $1"
     rm -rf $CLONE_TARGET
 
     if git clone "https://$1" $CLONE_TARGET --recurse; then
-        REPO_DIR="/var/lib/git_backup/temp/$1"
+        REPO_DIR="$TEMP_DIR/$1"
         mkdir -p $REPO_DIR
         mv -T $CLONE_TARGET $REPO_DIR
         echo "cloned $1" >> $LOG
@@ -28,7 +33,7 @@ clone_repo() {
 }
 pull_repo() {
     echo "pulling $1 if required"
-    pushd "/var/lib/git_backup/temp/$1" >/dev/null
+    pushd "$TEMP_DIR/$1" >/dev/null
 
     # check if pull is necessary -> cleaner logs
     git remote update
@@ -47,30 +52,46 @@ pull_repo() {
     popd >/dev/null
 }
 backup_repo() {
-    if [ -d "/var/lib/git_backup/temp/$1" ]; then
+    if [ -d "$TEMP_DIR/$1" ]; then
         pull_repo $1
     else
         clone_repo $1
     fi
 }
 
+backup_all_repos() {
+    echo "starting backup at $(date)" >> $LOG
+    while IFS="" read -r CUR_DIR || [ -n "$CUR_DIR" ]
+    do
+        # only lines with content
+        if [ -n "$CUR_DIR" ]; then
+            backup_repo $CUR_DIR
+        fi
+    # only load all repos when GITHUB_USERNAME given
+    done < <(cat \
+        $CONFIGS_DIR/other_repos.conf \
+        <([ -z ${GITHUB_USERNAME+x} ] || \
+        python3 $GIT_BACKUP_DIR/get_repos.py))
+    echo "backup complete at $(date)" >> $LOG
+    echo >> $LOG
+}
+
+create_borg_backup() {
+    borg check $BORG_REPO
+    echo "creating borg backup"
+    borg create -error --compression zstd,22 "${BORG_REPO}::git_backup_{now}" $TEMP_DIR
+
+    echo "pruning borg repo"
+    borg prune --keep-last --keep-montly $BORG_REPO
+
+    echo "compacting borg repo"
+    borg compact $BORG_REPO
+}
+
 echo "running git_backup.sh"
-echo "starting backup at $(date)" >> $LOG
 
-while IFS="" read -r CUR_DIR || [ -n "$CUR_DIR" ]
-do
-    # only lines with content
-    if [ -n "$CUR_DIR" ]; then
-        backup_repo $CUR_DIR
-    fi
-# only load all repos when GITHUB_USERNAME given
-done < <(cat \
-    /var/lib/git_backup/configs/other_repos.conf \
-    <([ -z ${GITHUB_USERNAME+x} ] || \
-    python3 /var/lib/git_backup/get_repos.py))
-
-echo "backup complete at $(date)" >> $LOG
-echo >> $LOG
+backup_all_repos
+create_borg_backup
 
 echo "all done"
 echo
